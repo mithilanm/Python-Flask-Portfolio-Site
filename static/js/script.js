250c8016e3695b0e687de430755d8f0fbc5504cb
mithilanm <mithilan_26@hotmail.com>
2020-07-07 13:53:07 -0400

    Active Tab on Nav Fix

--- static/js/script.js
+++ static/js/script.js
@@ -6,16 +6,3 @@ function myFunction() {
     x.className = "topnav";
   }
 }
-
-var btnContainer = document.getElementById("topnav");
-var btns = btnContainer.getElementsByClassName("btn");
-
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    if (current.length > 0) {
-      current[0].className = current[0].className.replace(" active", "");
-    }
-    this.className += " active";
-  });
-}
